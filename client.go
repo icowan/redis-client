ef62d76b223c1dffb92f1e3516bc4809eb6997b1
王聪 <congwang@creditease.cn>
2020-08-24 09:38:06 +0800

    add file

---
+++ client.go
@@ -0,0 +1,66 @@
+/**
+ * @Time: 2020/3/30 10:38
+ * @Author: solacowa@gmail.com
+ * @File: client
+ * @Software: GoLand
+ */
+
+package redis
+
+import (
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+type RedisInterface interface {
+	Set(k string, v interface{}, expir ...time.Duration) (err error)
+	Get(k string) (v string, err error)
+	Del(k string) (err error)
+	Exists(keys ...string) int64
+	HSet(k string, field string, v interface{}) (err error)
+	HGet(k string, field string) (res string, err error)
+	HGetAll(k string) (res map[string]string, err error)
+	HLen(k string) (res int64, err error)
+	ZCard(k string) (res int64, err error)
+	ZRangeWithScores(k string, start, stop int64) (res []redis.Z, err error)
+	ZAdd(k string, score float64, member interface{}) (err error)
+	HDelAll(k string) (err error)
+	HDel(k string, field string) (err error)
+	Keys(pattern string) (res []string, err error)
+	LLen(key string) int64
+	RPop(key string) (res string, err error)
+	LPush(key string, val interface{}) (err error)
+	TypeOf(key string) (res string, err error)
+	Close() error
+	Subscribe(channels ...string) *redis.PubSub
+	Publish(channel string, message interface{}) error
+	Incr(key string, exp time.Duration) error
+	SetPrefix(prefix string) RedisInterface
+	TTL(key string) time.Duration
+}
+
+const (
+	RedisCluster = "cluster"
+	RedisSingle  = "single"
+	expiration   = 600 * time.Second
+)
+
+func NewRedisClient(hosts, password, prefix string, db int) (RedisInterface, error) {
+	h := strings.Split(hosts, ",")
+	if len(h) > 1 {
+		return NewRedisCluster(
+			h,
+			password,
+			prefix,
+		), nil
+	}
+	return NewRedisSingle(
+		hosts,
+		password,
+		prefix,
+		db,
+	), nil
+
+}
